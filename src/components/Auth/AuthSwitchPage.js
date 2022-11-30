2321f08d7c04988e4b7ae7a490c47b9c9c6ccf29
EduardoPacheco12 <eduardooliveirapacheco131@gmail.com>
2022-11-30 17:36:34 -0300

    feat: add switch page text in Auth Components

---
+++ src/components/Auth/AuthSwitchPage.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+function AuthSwitchPage({ children }) {
+  return <Text>{children}</Text>;
+}
+
+export default AuthSwitchPage;
+
+const Text = styled.p`
+  font-family: "Roboto", sans-serif;
+  font-size: 15px;
+  font-weight: 500;
+  text-align: center;
+  color: #3daadc;
+  margin-top: 25px;
+  margin-left: 50px;
+  margin-right: 50px;
+`;
