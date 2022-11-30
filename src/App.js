5d57b5638915fdc4058970535efda4a70c079911
EduardoPacheco12 <eduardooliveirapacheco131@gmail.com>
2022-11-30 17:38:43 -0300

    feat: add Login Screen

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import "./assets/reset.css";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
-  return <div>Página aqui</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate replace to="/sign-in" />} />
+        <Route path="/sign-in" element={<Login />} />
+        <Route path="/sign-up" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
