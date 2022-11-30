9d2d58b3e6efe1815f58ca7d0626cb23bcf8560e
EduardoPacheco12 <eduardooliveirapacheco131@gmail.com>
2022-11-30 17:27:06 -0300

    feat: add input in Auth Components

---
+++ src/components/Auth/AuthInput.js
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+
+function AuthInput({ id, text, type, load, variable, setVariable }) {
+  return (
+    <Container>
+      <Name for={id}>{text}</Name>
+      <Input type={type} id={id} disabled={load ? true : false} onChange={(e) => setVariable(e.target.value)} value={variable} required />
+    </Container>
+  );
+}
+
+export default AuthInput;
+
+const Container = styled.div`
+  margin-top: 20px;
+  margin-left: 20px;
+`;
+
+const Name = styled.label`
+  font-family: "Roboto", sans-serif;
+  font-size: 15px;
+  font-weight: 500;
+  color: #3daadc;
+`;
+
+const Input = styled.input`
+  margin-top: 6px;
+  border: 2px solid #3daadc;
+  border-radius: 5px;
+  height: 35px;
+  width: 275px;
+`;
